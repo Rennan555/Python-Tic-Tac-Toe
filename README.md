664380af350653b762c6b26519df1fff565f88ea
Rennan555 <dungeonbr5@gmail.com>
2025-06-06 17:44:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Python Tic Tac Toe
+
+Python code for Tic Tac Toe game, using TKinter library fou GUI.
+
+## To Do list
+
+1. Draw condition
+2. GUI display
+3. GUI interaction scripts
+4. Basic AI implementation (Single Player mode) 
---
+++ src/table.py
@@ -0,0 +1,66 @@
+class Table:
+    def __init__(self):
+        self.X_ICON = 'X'
+        self.O_ICON = 'O'
+        self.spaces = [[None for _ in range(3)] for _ in range(3)]
+        
+    def __str__(self) -> str:
+        return str(self.spaces)
+    
+    def check_rows(self) -> bool:        
+        for i in range(3):
+            if self.spaces[i][0] == None: pass
+            elif self.spaces[i][0] == self.spaces[i][1] and self.spaces[i][1] == self.spaces[i][2]: return True
+        return False
+    
+    def check_columns(self) -> bool:
+        for i in range(3):
+            if self.spaces[0][i] == None: pass
+            elif self.spaces[0][i] == self.spaces[1][i] and self.spaces[1][i] == self.spaces[2][i]: return True
+        return False
+    
+    def check_diagonal_down(self) -> bool:
+        if self.spaces[0][0] == None: pass
+        elif self.spaces[0][0] == self.spaces[1][1] and self.spaces[1][1] == self.spaces[2][2]: return True
+        return False
+    
+    def check_diagonal_up(self) -> bool:
+        if self.spaces[0][2] == None: pass
+        elif self.spaces[0][2] == self.spaces[1][1] and self.spaces[1][1] == self.spaces[2][0]: return True
+        return False
+    
+    def check_victory_condition(self) -> str:
+        for i in range(3):
+            if self.check_rows(): return self.spaces[i][0] # Horizontal check
+            if self.check_columns(): return self.spaces[0][i] # Vertical check
+        if self.check_diagonal_down(): return self.spaces[0][0] # Diagonal down check
+        elif self.check_diagonal_up(): return self.spaces[0][2] # Diagonal up check
+        else: return None # Game continues / Draw
+
+# Test cases
+if __name__ == '__main__':
+    t = Table()
+    op = 7
+    match op:
+        case 0:
+            t.spaces[0][0] = t.O_ICON
+            t.spaces[0][1] = t.O_ICON
+            t.spaces[0][2] = t.O_ICON
+        case 1:
+            t.spaces[0][0] = t.O_ICON
+            t.spaces[1][0] = t.O_ICON
+            t.spaces[2][0] = t.O_ICON
+        case 2:
+            t.spaces[0][0] = t.O_ICON
+            t.spaces[1][1] = t.O_ICON
+            t.spaces[2][2] = t.O_ICON
+        case 3:
+            t.spaces[0][2] = t.O_ICON
+            t.spaces[1][1] = t.O_ICON
+            t.spaces[2][0] = t.O_ICON
+        case _:
+            t.spaces[0][0] = t.O_ICON
+            t.spaces[1][1] = t.O_ICON
+            t.spaces[0][2] = t.O_ICON
+    print(f'Table: {t}')
+    print(f'Victory: {t.check_victory_condition()}')
